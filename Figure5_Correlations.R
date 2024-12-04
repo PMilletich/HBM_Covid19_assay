14a729029a99bd269f06abe67a0331ed78685d34
Patricia Milletich <101075451+PMilletich@users.noreply.github.com>
2024-12-04 14:09:08 -0500

    Update Figure5_Correlations.R

--- Figure5_Correlations.R
+++ Figure5_Correlations.R
@@ -1,9 +1,32 @@
 library(ggplot2)
 library(ggpubr)
+library(bubbleHeatmap)
 
-setwd("C:/Users/PMilletich/OneDrive - University of Maryland School of Medicine/Desktop/Assorted/BM_Covid19/Final_Data.Scripts/")
 OG_data = read.csv("PostPandemic_75.csv")
+Corr_data = read.csv("Covid19_BM_Heatmap.csv")
 
+########################
+#Heatmap
+########################
+Corr = subset(Corr_data, Corr_data$Group == "Correlation")
+rownames(Corr) = Corr[,1]
+Corr$X = NULL; Corr$Group = NULL 
+Corr[] <- lapply(Corr, function(x) { as.numeric(as.character(x))})
+Corr_M = as.matrix(Corr)
+
+P = subset(Corr_data, Corr_data$Group == "P")
+P = P[-1,]
+rownames(P) = P[,1]
+P$X = NULL; P$Group = NULL 
+P[] <- lapply(P, function(x) { -log10(as.numeric(as.character(x)))})
+P_M = as.matrix(P)
+
+tree <-  bubbleHeatmap(Corr_M, P_M, 
+                       legendTitles = c("-log10(P)", "ρ"))
+
+################################################
+#Correlation Scatterplots 
+################################################
 A_Pseudo = ggplot(OG_data, aes(y = log10(Pseudo.IC50), x = log10(MSD.IgA.Spike)))+ 
   geom_point(shape = 21, size = 3, fill = "goldenrod") + 
   theme_bw() + 
@@ -32,6 +55,13 @@ GFP_FRNT = ggplot(OG_data, aes(y = log10(FRNT.eGFP.50), x = log10(MSD.IgA.Spike)
   geom_text(data = OG_data[1,], aes(y = log10(220), x = log10(18), 
              label = c("ρ=0.804\np<0.001"))); GFP_FRNT
 
+###################################################
+#Save Images 
+###################################################
+jpeg("Heatmap.jpeg", res = 400, height = 1500, width = 2000)
+grid.newpage()
+grid.draw(tree)
+dev.off()
 
 jpeg("IgA_Corr.jpeg", res = 400, height = 1500, width = 3000)
 ggarrange(A_Pseudo, A_FRNT,GFP_FRNT, ncol = 3)
